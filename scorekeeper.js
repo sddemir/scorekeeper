67436bdcb9c055ea5e68081a2694155910c98fae
Sümeyye Demir <sumeyyedemir1903@gmail.com>
2022-10-12 22:18:45 +0300

    progress

--- scorekeeper.js
+++ scorekeeper.js
@@ -3,6 +3,8 @@ const btn2 = document.querySelector('#p2button');
 const reset = document.querySelector('#reset');
 const score1= document.querySelector('#p1score');
 const score2= document.querySelector('#p2score');
+const select = document.querySelector('#numbers');
+const scores= document.querySelectorAll('span');
 
 let scoreOf1= 0;
 let scoreOf2= 0;
@@ -13,7 +15,9 @@ btn1.addEventListener('click', ()=>{
     if(!isGameOver){
         scoreOf1 += 1;
     if (scoreOf1 == winningScore){
-        isGameOver= true;}
+        isGameOver= true;
+    score1.classList.add('winner');
+    score2.classList.add('loser');}
     score1.textContent = scoreOf1;}
 })
 
@@ -21,14 +25,24 @@ btn2.addEventListener('click', ()=>{
     if(!isGameOver){
         scoreOf2 += 1;
     if (scoreOf2 == winningScore){
-        isGameOver= true;}
+        isGameOver= true;
+        score2.classList.add('winner');
+        score1.classList.add('loser');}
     score2.textContent = scoreOf2;}
 })
 
-reset.addEventListener('click', ()=> {
+select.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    resett();
+})
+
+reset.addEventListener('click', resett)
+function resett () {
     isGameOver = false;
     scoreOf1 = 0;
     scoreOf2 = 0;
     score1.textContent= 0;
     score2.textContent= 0;
-})
+    score1.classList.remove('winner', 'loser');
+    score2.classList.remove('winner', 'loser');
+}
